4398a8ef7666f7864e50bf2acf0954a6554d7433
MVVSAIKUMAR <mvvsaikumar9@gmail.com>
2022-04-03 10:38:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/* const heading = document.getElementsByTagName("he");
+console.log(heading[0].innerText)
+
+const heading2=document.getElementsByClassName("he");
+console.log(heading2[0].innerText); */
+
+function handleSubmit() {
+    const click = document.getElementById("main")
+    click.innerText="mernstackstudent";
+}
+
+/* function handleSubmit () { 
+    var userInput = document.getElementById("name");
+    var todo = document.createElement("h1");
+    todo.innerText = userInput.value;
+    todo.setAttribute("class", "todoDiv");
+    document.getElementById("todoData").appendChild(todo);
+} */
+
+// adding dynamic html 
+// create an empty div tag and give it some id.
+// capture the input field value
+// create the element that you want to add in html.
+// append the element inside the empty div tag.
